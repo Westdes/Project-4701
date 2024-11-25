54ca7b8d84e3b087e1c749b1d944da0fb4178046
Westdes <edwardhui1900@gmail.com>
2024-11-26 00:36:49 +0800

    Add Part 1 todo code

--- object_tracking/scripts/object_tracking_node.py
+++ object_tracking/scripts/object_tracking_node.py
@@ -40,7 +40,7 @@ Arm_X = 500
 Arm_Y = 100
 arm_x = Arm_X
 arm_y = Arm_Y
-x_pid = PID.PID(P=0.70, I=0.0001, D=0.0001) 
+x_pid = PID.PID(P=0.70, I=0.0001, D=0.0001)
 y_pid = PID.PID(P=1.30, I=0.0005, D=0.0001)
 arm_x_pid = PID.PID(P=0.1, I=0.0001, D=0.0001)  # pid initialize
 arm_y_pid = PID.PID(P=0.18, I=0.0005, D=0.0001)
@@ -55,19 +55,22 @@ range_rgb = {
 }
 
 # Init position
+
+
 def initMove(delay=True):
     with lock:
         target = ik.setPitchRanges((0.0, 0.12, 0.08), -145, -180, 0)
         if target:
             servo_data = target[1]
             bus_servo_control.set_servos(joints_pub, 1500, ((1, 200), (2, 500), (3, arm_y),
-                                        (4, servo_data['servo4']),(5, servo_data['servo5']),(6, arm_x)))
+                                                            (4, servo_data['servo4']), (5, servo_data['servo5']), (6, arm_x)))
     if delay:
         rospy.sleep(2)
 
+
 def off_rgb():
     global rgb_pub
-    
+
     led = Led()
     led.index = 0
     led.rgb.r = 0
@@ -78,10 +81,12 @@ def off_rgb():
     rgb_pub.publish(led)
 
 # Reset variables
+
+
 def reset():
     global target_color
     global arm_x, arm_y
-    
+
     with lock:
         arm_x = Arm_X
         arm_y = Arm_Y
@@ -93,21 +98,25 @@ def reset():
         target_color = 'None'
 
 # app initialization
+
+
 def init():
     rospy.loginfo("object tracking Init")
     initMove()
     reset()
 
 # image processing callback
+
+
 def run(msg):
     global lock
     global move
     global arm_x, arm_y
-    
+
     center_x = msg.center_x
     center_y = msg.center_y
     radius = msg.data
-    
+
     with lock:
         if __isRunning:
             if center_x > 0 and center_y > 0:
@@ -117,65 +126,84 @@ def run(msg):
                 # set the pid point position in the x direction
                 # update x pid
                 # update the robotic arm x
-                
+                x_pid.SetPoint = img_w/2.0
+                x_pid.update(center_x)
+                arm_x += x_pid.output
+
                 # TODO:robot arm y axis tracking
                 if abs(center_y - img_h/2.0) < 15:
                     center_y = img_h/2.0
                 # set the point position in the y direction
                 # update y pid
                 # update the robotic arm y
-                
+                y_pid.SetPoint = img_h/2.0
+                y_pid.update(center_y)
+                arm_y += y_pid.output
+
                 # TODO:move the robotic arm
-                
+                target = ik.setPitchRanges((0, 0.12, 0.08), -145, -180, 0)
+                if target:
+                    servo_data = target[1]
+                    bus_servo_control.set_servos(
+                        joints_pub, 20, ((3, arm_y), (6, arm_x)))
+
                 # TODO:chassis x axis tracking
                 if abs(arm_x - Arm_X) < 5:
                     arm_x = Arm_X
                 # set the point position in the x direction
                 # update x pid
                 # update the chassis x
-                
-                
+                arm_x_pid.SetPoint = Arm_X
+                arm_x_pid.update(arm_x)
+                x_velocity = arm_x_pid.output
+
                 # TODO:chassis x axis tracking
                 if abs(arm_y - Arm_Y) < 5:
                     arm_y = Arm_Y
                 # set the point position in the y direction
                 # update y pid
                 # update the chassis y
-                
+
                 # TODO:move the chassis
-                
+                set_translation.publish(x_velocity, y_velocity)
+
                 move = True
-                        
+
             else:
                 if move:
                     move = False
                     rospy.sleep(0.1)
-                    set_translation.publish(0,0)
-             
+                    set_translation.publish(0, 0)
+
 
 result_sub = None
 heartbeat_timer = None
 # enter callback
+
+
 def enter_func(msg):
     global lock
     global result_sub
-    
+
     rospy.loginfo("enter object tracking")
     init()
     with lock:
         if result_sub is None:
             rospy.ServiceProxy('/visual_processing/enter', Trigger)()
-            result_sub = rospy.Subscriber('/visual_processing/result', Result, run)
-            
+            result_sub = rospy.Subscriber(
+                '/visual_processing/result', Result, run)
+
     return [True, 'enter']
 
 # exit function
+
+
 def exit_func(msg):
     global lock
     global result_sub
     global __isRunning
     global heartbeat_timer
-    
+
     rospy.loginfo("exit object tracking")
     with lock:
         __isRunning = False
@@ -190,46 +218,54 @@ def exit_func(msg):
                 heartbeat_timer = None
         except BaseException as e:
             rospy.loginfo('%s', e)
-        
+
     return [True, 'exit']
 
 # start
+
+
 def start_running():
     global lock
     global __isRunning
-    
+
     rospy.loginfo("start running object tracking")
     with lock:
         __isRunning = True
 
 # stop
+
+
 def stop_running():
     global lock
     global __isRunning
-    
+
     rospy.loginfo("stop running object tracking")
     with lock:
         reset()
         __isRunning = False
         initMove(delay=False)
-        set_velocity.publish(0,0,0)
+        set_velocity.publish(0, 0, 0)
         rospy.ServiceProxy('/visual_processing/set_running', SetParam)()
 
 # set_running service callback
+
+
 def set_running(msg):
-    
+
     if msg.data:
         start_running()
     else:
         stop_running()
-        
+
     return [True, 'set_running']
 
 # set_target service callback
+
+
 def set_target(msg):
     global lock
     global target_color
-    
+
     rospy.loginfo("%s", msg)
     with lock:
         target_color = msg.data
@@ -242,20 +278,24 @@ def set_target(msg):
         led.index = 1
         rgb_pub.publish(led)
         rospy.sleep(0.1)
-        visual_running = rospy.ServiceProxy('/visual_processing/set_running', SetParam)
-        visual_running('color',target_color)
+        visual_running = rospy.ServiceProxy(
+            '/visual_processing/set_running', SetParam)
+        visual_running('color', target_color)
         rospy.sleep(0.1)
-        
+
     return [True, 'set_target']
 
 # heartbeat service callback
+
+
 def heartbeat_srv_cb(msg):
     global heartbeat_timer
 
     if isinstance(heartbeat_timer, Timer):
         heartbeat_timer.cancel()
     if msg.data:
-        heartbeat_timer = Timer(5, rospy.ServiceProxy('/object_tracking/exit', Trigger))
+        heartbeat_timer = Timer(5, rospy.ServiceProxy(
+            '/object_tracking/exit', Trigger))
         heartbeat_timer.start()
     rsp = SetBoolResponse()
     rsp.success = msg.data
@@ -267,35 +307,45 @@ if __name__ == '__main__':
     # init node
     rospy.init_node('object_tracking', log_level=rospy.DEBUG)
     # publish servo
-    joints_pub = rospy.Publisher('/servo_controllers/port_id_1/multi_id_pos_dur', MultiRawIdPosDur, queue_size=1)
+    joints_pub = rospy.Publisher(
+        '/servo_controllers/port_id_1/multi_id_pos_dur', MultiRawIdPosDur, queue_size=1)
     # app communication
     enter_srv = rospy.Service('/object_tracking/enter', Trigger, enter_func)
     exit_srv = rospy.Service('/object_tracking/exit', Trigger, exit_func)
-    # TODO: init the running service
 
-    set_target_srv = rospy.Service('/object_tracking/set_target', SetTarget, set_target)
-    heartbeat_srv = rospy.Service('/object_tracking/heartbeat', SetBool, heartbeat_srv_cb)
+    # TODO: init the running service
+    running_srv = rospy.Service(
+        '/object_tracking/set_running', SetBool, set_running)
+
+    set_target_srv = rospy.Service(
+        '/object_tracking/set_target', SetTarget, set_target)
+    heartbeat_srv = rospy.Service(
+        '/object_tracking/heartbeat', SetBool, heartbeat_srv_cb)
+
     # TODO: init the chassis control publisher
-    
+    set_translation = rospy.Publisher(
+        '/chassis_control/set_translation', SetTranslation, queue_size=1)
+    set_velocity = rospy.Publisher(
+        '/chassis_control/set_velocity', SetVelocity, queue_size=1)
+
     # buzzer control
     buzzer_pub = rospy.Publisher('/sensor/buzzer', Float32, queue_size=1)
     # rgb leb control
     rgb_pub = rospy.Publisher('/sensor/rgb_led', Led, queue_size=1)
-    rospy.sleep(0.5) # delay for publisher
+    rospy.sleep(0.5)  # delay for publisher
 
     debug = False
     if debug:
         rospy.sleep(0.2)
         enter_func(1)
-        
+
         msg = SetTarget()
         msg.data = 'red'
-        
+
         set_target(msg)
         start_running()
-    
+
     try:
         rospy.spin()
     except KeyboardInterrupt:
         rospy.loginfo("Shutting down")
-
--- visual_processing/scripts/visual_processing_node.py
+++ visual_processing/scripts/visual_processing_node.py
@@ -9,7 +9,7 @@ import rospy
 import numpy as np
 from armpi_pro import Misc
 from armpi_pro import apriltag
-from threading import RLock,Timer
+from threading import RLock, Timer
 from std_srvs.srv import *
 from std_msgs.msg import *
 from sensor_msgs.msg import Image
@@ -54,50 +54,57 @@ def getAreaMaxContour(contours):
 
 # apriltag detection
 detector = apriltag.Detector(searchpath=apriltag._get_demo_searchpath())
+
+
 def apriltag_Detect(img):
     global pub_time
     global publish_en
     global id_smallest
-    
+
     msg = Result()
     img_copy = img.copy()
     img_h, img_w = img.shape[:2]
-    frame_resize = cv2.resize(img_copy, size_m, interpolation=cv2.INTER_NEAREST)
+    frame_resize = cv2.resize(
+        img_copy, size_m, interpolation=cv2.INTER_NEAREST)
     gray = cv2.cvtColor(frame_resize, cv2.COLOR_BGR2GRAY)
     detections = detector.detect(gray, return_image=False)
-    
+
     if len(detections) != 0:
         for i, detection in enumerate(detections):
             tag_id = int(detection.tag_id)        # get tag_id
             corners = np.rint(detection.corners)  # get four corners
 
             # TODO: map the four corners from the size_m to image size
+            corners = corners * (img_w/size_m[0], img_h/size_m[1])
 
             # TODO: get the center point (object_center_x, object_center_y, in image size)
             # Hint: use detection.center
+            object_center_x = int(detection.center[0] * img_w/size_m[0])
+            object_center_y = int(detection.center[1] * img_h/size_m[1])
 
-            object_angle = int(math.degrees(math.atan2(corners[0][1] - corners[1][1], corners[0][0] - corners[1][0])))  # rotation angle
+            object_angle = int(math.degrees(math.atan2(
+                corners[0][1] - corners[1][1], corners[0][0] - corners[1][0])))  # rotation angle
             if id_smallest == 'None' or tag_id <= id_smallest:
-                id_smallest = tag_id        
+                id_smallest = tag_id
                 msg.center_x = object_center_x
                 msg.center_y = object_center_y
                 msg.angle = object_angle
                 msg.data = id_smallest
-                
+
         id_smallest = 'None'
-        publish_en = True 
-    
+        publish_en = True
+
     if publish_en:
         if (time.time()-pub_time) >= 0.06:
             result_pub.publish(msg)  # publish
             pub_time = time.time()
-            
+
         if msg.data == 0:
             publish_en = False
             result_pub.publish(msg)
-                
+
     return img
-    
+
 
 # [ROI, weight]
 roi = [(50, 70,  0, 160, 0.2),
@@ -110,66 +117,92 @@ roi_h3 = roi[2][0] - roi[1][0]
 roi_h_list = [roi_h1, roi_h2, roi_h3]
 
 # color detection
+
+
 def color_detect(img, color):
     global pub_time
     global publish_en
     global color_range_list
-    
+
     if color == 'None':
         return img
-    
+
     msg = Result()
     area_max = 0
     area_max_contour = 0
     img_copy = img.copy()
     img_h, img_w = img.shape[:2]
-    frame_resize = cv2.resize(img_copy, size_m, interpolation=cv2.INTER_NEAREST)
-    frame_lab = cv2.cvtColor(frame_resize, cv2.COLOR_BGR2LAB)  # transfer image to LAB space
+    frame_resize = cv2.resize(
+        img_copy, size_m, interpolation=cv2.INTER_NEAREST)
+    # transfer image to LAB space
+    frame_lab = cv2.cvtColor(frame_resize, cv2.COLOR_BGR2LAB)
 
     if color in color_range_list:
         # TODO:define the frame_mask for the color (set the pixel inrange to 255 others to 0)
+        frame_mask = cv2.inRange(frame_lab,
+                                 (color_range_list[color]['min'][0],
+                                  color_range_list[color]['min'][1],
+                                  color_range_list[color]['min'][2]),
+                                 (color_range_list[color]['max'][0],
+                                  color_range_list[color]['max'][1],
+                                  color_range_list[color]['max'][2]))
 
-        eroded = cv2.erode(frame_mask, cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)))          # erosion
-        dilated = cv2.dilate(eroded, cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2)))            # dilation
-        contours = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]         # find all contours
-        area_max_contour, area_max = getAreaMaxContour(contours)                                   # find the largest contours
+        eroded = cv2.erode(frame_mask, cv2.getStructuringElement(
+            cv2.MORPH_RECT, (2, 2)))          # erosion
+        dilated = cv2.dilate(eroded, cv2.getStructuringElement(
+            cv2.MORPH_RECT, (2, 2)))            # dilation
+        contours = cv2.findContours(
+            dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]         # find all contours
+        area_max_contour, area_max = getAreaMaxContour(
+            contours)                                   # find the largest contours
 
         if area_max > 100:  # find largest area
-            
+
             # TODO: find the smallest incircle center x, center y, radius from area_max_contour
             # Hint: cv2.minEnclosingCircle
+            (center_x, center_y), radius = cv2.minEnclosingCircle(area_max_contour)
+            center_x = int(center_x * img_w/size_m[0])
+            center_y = int(center_y * img_h/size_m[1])
+            radius = int(radius * img_w/size_m[0])
+            msg.center_x = center_x
+            msg.center_y = center_y
+            msg.angle = 0
+            msg.data = radius
 
             publish_en = True
-        
+
         if publish_en:
             if (time.time()-pub_time) >= 0.06:
                 result_pub.publish(msg)  # publish
                 pub_time = time.time()
-                
+
             if msg.data == 0:
                 publish_en = False
                 result_pub.publish(msg)
-        
+
     return img
 
 # camera image callback
+
+
 def image_callback(ros_image):
     global lock
-    global target_type 
+    global target_type
     global target_color
-    
+
     # transfer the message to image
-    image = np.ndarray(shape=(ros_image.height, ros_image.width, 3), dtype=np.uint8, buffer=ros_image.data)  
+    image = np.ndarray(shape=(ros_image.height, ros_image.width,
+                       3), dtype=np.uint8, buffer=ros_image.data)
     cv2_img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
+
     frame_result = cv2_img
     with lock:
         if __isRunning:
-            if target_type == 'apriltag': 
+            if target_type == 'apriltag':
                 frame_result = apriltag_Detect(cv2_img)
-            elif target_type == 'color': 
-                frame_result = color_detect(cv2_img,target_color)
-            
+            elif target_type == 'color':
+                frame_result = color_detect(cv2_img, target_color)
+
     rgb_image = cv2.cvtColor(frame_result, cv2.COLOR_BGR2RGB).tostring()
     ros_image.data = rgb_image
     image_pub.publish(ros_image)
@@ -181,27 +214,34 @@ def init():
     global id_smallest
     global target_color
     global color_range_list
-    
+
     publish_en = True
     id_smallest = 'None'
     target_color = 'None'
     rospy.loginfo("visual processing Init")
-    color_range_list = rospy.get_param('/lab_config_manager/color_range_list', {})  # get lab range from ros param server
+    # get lab range from ros param server
+    color_range_list = rospy.get_param(
+        '/lab_config_manager/color_range_list', {})
+
 
 image_sub_st = False
+
+
 def enter_func(msg):
     global lock
     global image_sub
     global image_sub_st
-    
+
     rospy.loginfo("enter visual processing")
     init()
     with lock:
         if not image_sub_st:
             image_sub_st = True
-            image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, image_callback)
-            
+            image_sub = rospy.Subscriber(
+                '/usb_cam/image_raw', Image, image_callback)
+
     return [True, 'enter']
+
 
 def exit_func(msg):
     global lock
@@ -209,7 +249,7 @@ def exit_func(msg):
     global image_sub_st
     global __isRunning
     global target_color
-    
+
     rospy.loginfo("exit visual processing")
     with lock:
         __isRunning = False
@@ -220,29 +260,32 @@ def exit_func(msg):
                 image_sub.unregister()
         except BaseException as e:
             rospy.loginfo('%s', e)
-        
+
     return [True, 'exit']
+
 
 def start_running():
     global lock
     global __isRunning
-    
+
     rospy.loginfo("start running visual processing")
     with lock:
         __isRunning = True
 
+
 def stop_running():
     global lock
     global __isRunning
-    
+
     rospy.loginfo("stop running visual processing")
     with lock:
         __isRunning = False
 
+
 def set_running(msg):
-    global target_type 
+    global target_type
     global target_color
-    
+
     rospy.loginfo("%s", msg)
     init()
     if msg.type:
@@ -252,7 +295,7 @@ def set_running(msg):
     else:
         target_color = 'None'
         stop_running()
-        
+
     return [True, 'set_running']
 
 
@@ -260,12 +303,16 @@ if __name__ == '__main__':
     # init node
     rospy.init_node('visual_processing', log_level=rospy.DEBUG)
     # communication
-    image_pub = rospy.Publisher('/visual_processing/image_result', Image, queue_size=1)
+    image_pub = rospy.Publisher(
+        '/visual_processing/image_result', Image, queue_size=1)
     # TODO: init the result_pub for publish the result
-    
+    result_pub = rospy.Publisher(
+        '/visual_processing/result', Result, queue_size=1)
+
     enter_srv = rospy.Service('/visual_processing/enter', Trigger, enter_func)
     exit_srv = rospy.Service('/visual_processing/exit', Trigger, exit_func)
-    running_srv = rospy.Service('/visual_processing/set_running', SetParam, set_running)
+    running_srv = rospy.Service(
+        '/visual_processing/set_running', SetParam, set_running)
 
     try:
         rospy.spin()
@@ -273,4 +320,3 @@ if __name__ == '__main__':
         rospy.loginfo("Shutting down")
     finally:
         cv2.destroyAllWindows()
-
